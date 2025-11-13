f9aba67d7c83fc8f421ff9978427a1d4e843627a
kdonati <kdonati@indeed.com>
2025-11-13 12:24:51 -0800

    saving changes

--- models/warehouse/fact_customer_order_monthly.sql
+++ models/warehouse/fact_customer_order_monthly.sql
@@ -8,17 +8,17 @@ with fo as (
 
 , setup as (
         select
-            dc.customer_id
+            dc.customer_unique_id
             , YEAR(fo.ordered_at) as order_year
             , MONTH(fo.ordered_at) as order_month
             , fo.order_count
         from dc
         left join fo
-            on dc.customer_id = fo.customer_id
+            on dc.customer_unique_id = fo.customer_id
 )
 
 select
-    customer_id
+    customer_unique_id
     , order_year
     , order_month
     , sum(order_count) as monthly_order_count
